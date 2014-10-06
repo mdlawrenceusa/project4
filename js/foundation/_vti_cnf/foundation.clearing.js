a46e31f54b073babf2b1e4cfc069673c7b1d8851
Michael Lawrence <mdlawrence@me.com>
2014-10-05 22:16:24 -0400

    Initial Commit
    Initial commit of completed Screen Cast

---
+++ js/foundation/_vti_cnf/foundation.clearing.js
@@ -0,0 +1,13 @@
+vti_encoding:SR|utf8-nl
+vti_syncwith_localhost\\x\:\\et710\\ewupgrade/x\:/et710/ewupgrade:TW|02 Oct 2014 03:32:25 -0000
+vti_timelastmodified:TR|06 Oct 2014 01:00:24 -0000
+vti_author:SR|MICHAELLAWR9EFD\\michaellawrence
+vti_modifiedby:SR|MICHAELLAWR9EFD\\michaellawrence
+vti_nexttolasttimemodified:TW|02 Oct 2014 03:32:25 -0000
+vti_timecreated:TR|04 Oct 2014 01:40:27 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|
+vti_syncwith_146.111.120.96\:80/fa14/et_710_c1/projects:TX|04 Oct 2014 01:40:26 -0000
+vti_syncofs_146.111.120.96\:80/fa14/et_710_c1/projects:TW|06 Oct 2014 01:43:29 -0000
+vti_cacheddtm:TX|04 Oct 2014 01:40:26 -0000
+vti_filesize:IR|16357
---
+++ js/vendor/_vti_cnf/modernizr.js
@@ -0,0 +1,13 @@
+vti_encoding:SR|utf8-nl
+vti_syncwith_localhost\\x\:\\et710\\ewupgrade/x\:/et710/ewupgrade:TW|02 Oct 2014 03:32:34 -0000
+vti_timelastmodified:TR|06 Oct 2014 01:00:25 -0000
+vti_author:SR|MICHAELLAWR9EFD\\michaellawrence
+vti_modifiedby:SR|MICHAELLAWR9EFD\\michaellawrence
+vti_nexttolasttimemodified:TW|02 Oct 2014 03:32:34 -0000
+vti_timecreated:TR|04 Oct 2014 01:40:33 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|project4/default.html datatables/master.dwt datatables/angulardemo.html project4/index2.html datatables/index2.html datatables/index.html project4/master_4.dwt
+vti_syncwith_146.111.120.96\:80/fa14/et_710_c1/projects:TX|04 Oct 2014 01:40:34 -0000
+vti_syncofs_146.111.120.96\:80/fa14/et_710_c1/projects:TW|06 Oct 2014 01:43:39 -0000
+vti_cacheddtm:TX|04 Oct 2014 01:40:34 -0000
+vti_filesize:IR|11262
